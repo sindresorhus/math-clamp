1b8369b52a00fdf8369567c96d93f33cd57ff09e
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2015-07-20 14:06:20 +0600

    Make things more explicit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 'use strict';
+
 module.exports = function (x, min, max) {
 	if (min > max) {
 		throw new RangeError('`min` should be lower than `max`');
 	}
 
-	return x < min ? min : x > max ? max : x;
+	if (x < min) {
+		x = min;
+	}
+
+	if (x > max) {
+		x = max;
+	}
+
+	return x;
 };
