7f471363558c14dbadedf4937fbadb2075f5fc3f
Richie Bendall <richiebendall@gmail.com>
2024-01-18 20:31:28 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 export default function mathClamp(number, {min, minimum, max, maximum}) {
-	if (min !== undefined) {
-		if (minimum === undefined) {
-			minimum = min;
+	if (minimum !== undefined) {
+		if (min === undefined) {
+			min = minimum;
 		} else {
 			throw new TypeError('`min` and `minimum` are mutually exclusive');
 		}
 	}
 
-	if (max !== undefined) {
-		if (maximum === undefined) {
-			maximum = max;
+	if (maximum !== undefined) {
+		if (max === undefined) {
+			max = maximum;
 		} else {
 			throw new TypeError('`max` and `maximum` are mutually exclusive');
 		}
 	}
 
-	if (minimum > maximum) {
+	if (min > max) {
 		throw new RangeError('Minimum should be lower than maximum');
 	}
 
-	if (number < minimum) {
+	if (number < min) {
 		return minimum;
 	}
 
-	if (number > maximum) {
+	if (number > max) {
 		return maximum;
 	}
 
