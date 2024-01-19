105ef7ccd478233d02b7d3a4d70a24fee717b989
Richie Bendall <richiebendall@gmail.com>
2024-01-19 15:15:48 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export default function mathClamp(number, {min, minimum, max, maximum}) {
+	// TODO: Remove `minimum` and `maximum` options in the next breaking release
 	min ??= minimum;
 	max ??= maximum;
 
